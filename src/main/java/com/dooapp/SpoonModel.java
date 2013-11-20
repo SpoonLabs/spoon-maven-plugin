eebe681fc51504654a84aac57d2bcae6ac0b512b
christophe <christophe@dooapp.com>
2013-11-20 11:02:13 +0100

    First commit

---
+++ src/main/java/com/dooapp/SpoonModel.java
@@ -0,0 +1,37 @@
+package com.dooapp;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+
+/**
+ * Simple class to store data loaded from XML config file
+ * <br>
+ * Created at 18/11/2013 17:39.<br>
+ *
+ * @author Christophe DUFOUR
+ */
+@XmlType(name = "spoonModel")
+@XmlRootElement(name = "spoonModel")
+public class SpoonModel {
+    private List<String> templates;
+    private List<String> processors;
+    public List<String> getProcessors() {
+        return processors;
+    }
+    @XmlElementWrapper(name = "processors")
+    @XmlElement(name = "processor")
+    public void setProcessors(List<String> processors) {
+        this.processors = processors;
+    }
+    public List<String> getTemplates() {
+        return templates;
+    }
+    @XmlElementWrapper(name = "templates")
+    @XmlElement(name = "template")
+    public void setTemplates(List<String> templates) {
+        this.templates = templates;
+    }
+}
---
+++ src/test/java/com/dooapp/XMLLoaderTest.java
@@ -0,0 +1,15 @@
+package com.dooapp;
+
+import org.junit.Test;
+
+/**
+ * Created at 19/11/2013 10:14.<br>
+ *
+ * @author Christophe DUFOUR
+ */
+public class XMLLoaderTest {
+    @Test
+    public void simpleTest() throws Exception {
+        SpoonModel model = XMLLoader.load(XMLLoaderTest.class.getResourceAsStream("spoon.xml"));
+    }
+}
