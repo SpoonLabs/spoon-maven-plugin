00cf26523a1ebc8f0ce9c73987afd1b5a3c46fd0
Gerard Paligot <gerard.paligot@gmail.com>
2014-10-29 10:05:31 +0100

    Supports processors when the plugin spoon a project.

--- src/main/java/com/dooapp/configuration/SimpleSpoonConfiguration.java
+++ src/main/java/com/dooapp/configuration/SimpleSpoonConfiguration.java
@@ -4,6 +4,7 @@ import com.dooapp.Spoon;
 import com.dooapp.logging.ReportBuilder;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Created by gerard on 15/10/2014.
@@ -17,7 +18,9 @@ class SimpleSpoonConfiguration extends AbstractSpoonConfigurationBuilder {
 
 	@Override
 	public SpoonConfigurationBuilder addProcessors() {
-		final String[] processors = spoon.getProcessors();
+		final List<String> listProcessors = spoon.getProcessorsPath();
+		String[] processors = listProcessors.toArray(
+				new String[listProcessors.size()]);
 		if (processors != null && processors.length != 0) {
 			parameters.add("-p");
 			parameters.add(implode(processors, File.pathSeparator));
