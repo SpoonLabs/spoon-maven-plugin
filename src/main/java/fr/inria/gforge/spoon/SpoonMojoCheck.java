9c71ebff7ec68392786130b68a96e5f25ed49e0f
Simon Urli <simon.urli@inria.fr>
2018-01-22 16:37:11 +0100

    With check goal, plugin must compile original sources

--- src/main/java/fr/inria/gforge/spoon/SpoonMojoCheck.java
+++ src/main/java/fr/inria/gforge/spoon/SpoonMojoCheck.java
@@ -2,17 +2,25 @@ package fr.inria.gforge.spoon;
 
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import java.io.File;
 
 @SuppressWarnings("UnusedDeclaration")
 @Mojo(
 		name = "check",
 		defaultPhase = LifecyclePhase.VERIFY,
 		requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
-public class SpoonMojoCheck extends SpoonMojoGenerate {
 
+public class SpoonMojoCheck extends SpoonMojoGenerate {
 	@Override
 	public String getOutputType() {
 		return "nooutput";
+	}
+
+	@Override
+	public boolean isCompileOriginalSources() {
+		return true;
 	}
 }
