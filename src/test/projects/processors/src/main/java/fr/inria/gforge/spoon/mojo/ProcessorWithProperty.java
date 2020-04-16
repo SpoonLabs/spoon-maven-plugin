c57735263491b2294dfac2cc90af6c61b0a04ab9
Martin Monperrus <monperrus@users.noreply.github.com>
2020-04-16 10:34:10 +0200

    update to Spoon 8.1 (#67)

--- src/test/projects/processors/src/main/java/fr/inria/gforge/spoon/mojo/ProcessorWithProperty.java
+++ src/test/projects/processors/src/main/java/fr/inria/gforge/spoon/mojo/ProcessorWithProperty.java
@@ -8,10 +8,9 @@ import spoon.reflect.declaration.CtClass;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by urli on 21/09/2017.
- */
 public class ProcessorWithProperty extends AbstractProcessor<CtClass> {
+    @Property
+    String methods;
 
     @Property
     String oldClassName;
@@ -30,6 +29,7 @@ public class ProcessorWithProperty extends AbstractProcessor<CtClass> {
 
     @Override
     public void process(CtClass element) {
+        System.out.println(methods);
         if (!(comments != null && comments.size() == 2 && comments.get(0).equals("hello") && comments.get(1).equals("hello, world"))) {
             element.addComment(this.getFactory().createInlineComment("Content comments: "+ StringUtils.join(comments.iterator(),",")));
             return;
